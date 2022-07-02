29e4617ab149c9b3c018168f7868d7a647fd8584
Utkarsh <52343042+utkarsh-chaurasia@users.noreply.github.com>
2022-07-03 02:05:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let movies = [{
         name: "falcon and the winter soldier",
         des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-        image: "images/slider 2.PNG"
+        image: "images/slider 2.png"
     },
     {
         name: "loki",
